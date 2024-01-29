39027b8fc6fe3884372df033922625a902f3d484
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-01-29 07:54:34 +0530

    Time: 491 ms (43.21%) | Memory: 220.6 MB (11.67%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findans(int i, bool buy, int no, vector<int> &v, vector<vector<vector<int>>> &dp)
+    {
+        if (i == v.size() || !no)
+        {
+            return 0;
+        }
+        if (dp[i][buy][no] != -1)
+        {
+            return dp[i][buy][no];
+        }
+        if (buy)
+        {
+            return dp[i][buy][no] = max(findans(i + 1, 0, no, v, dp) - v[i], findans(i + 1, 1, no, v, dp));
+        }
+        else
+        {
+            return dp[i][buy][no] = max(findans(i + 1, 1, no - 1, v, dp) + v[i], findans(i + 1, 0, no, v, dp));
+        }
+    }
+    int maxProfit(vector<int> &v)
+    {
+        vector<vector<vector<int>>> dp(v.size() + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        return findans(0, 1, 2, v, dp);
+    }
+};
