ec2a37bb763f12d5a9b6565494e2bb9c5eb9e61d
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-02-05 08:03:17 +0530

    Time: 51 ms (55.64%) | Memory: 49.1 MB (7.49%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        var h = "Hello World"
+        return h;
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
