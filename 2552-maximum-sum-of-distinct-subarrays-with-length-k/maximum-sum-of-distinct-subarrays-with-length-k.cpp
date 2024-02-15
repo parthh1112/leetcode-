5afb4a92ec2453781018742f8ba39fe3c192a153
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-02-15 19:14:16 +0530

    Time: 201 ms (43.79%) | Memory: 102.5 MB (16.79%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,26 +1,18 @@
+#define ll long long
 class Solution {
 public:
-    long long maximumSubarraySum(vector<int>& v, int k) {
-        long long int ans = 0;
-        long long int i,sum,j;i=j=sum=0;
-        unordered_map<long long int,long long int> m;
-        while(i < v.size()){
-            bool chk = 0;
+    ll maximumSubarraySum(vector<int>& v, int k) { 
+        ll int i,sum,ans,j;i=j=sum=ans=0;
+        unordered_map<ll int,ll int> m;
+        while(i < v.size()){ 
             while((j < v.size()) && ((j-i) < k)){
                 sum += v[j];
                 m[v[j]]++;
                 j++;                
             }
-            if(m.size() == k){
-                ans = max(ans,sum);
-                chk = 1;cout<<"ok";
-            }  
-            if(m[v[i]] == 1){
-                m.erase(v[i]);
-            }
-            else{
-                m[v[i]]--;
-            }
+            if(m.size() == k) ans = max(ans,sum);
+            if(m[v[i]] == 1) m.erase(v[i]);
+            else m[v[i]]--;
             sum -= v[i];  
             i++;
         }
