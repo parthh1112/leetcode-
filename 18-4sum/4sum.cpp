cc966725ecd4de45d8a5f0de9e9b5c86f86892f0
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-02-24 15:02:08 +0530

    Time: 26 ms (72.03%) | Memory: 15.8 MB (57.95%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& v, int t) {
         if(v.size() < 4)return {}; 
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         sort(v.begin(),v.end());
         for(int i=0;i<v.size()-3;i++){
+            if(i > 0 && v[i] == v[i-1])continue;
             for(int j=i+1;j<v.size()-2;j++){
+                if(j != i+1 && v[j] == v[j-1])continue;
                 int k = j+1;
                 int l = v.size() - 1;
                 while(k < l && k < v.size()-1 && l > 0){
                 long int sum = v[i];
                     sum += v[j];sum += v[k];sum += v[l]; 
                     if(sum == t){
-                        vector<int> q {v[i],v[j],v[k],v[l]}; 
-                        ans.insert(q); 
+                        ans.push_back({v[i],v[j],v[k],v[l]}); 
                         k++;l--; 
+                        while(k < l && v[k] == v[k-1])k++;
+                        while(k < l && v[l] == v[l+1])l--;
                     }
                     else if(sum > t) l--;
                     else k++;
                 }
             }
         }
-        vector<vector<int>> fans (ans.begin(),ans.end());
-        return fans;
+        return ans; 
     }
 };
