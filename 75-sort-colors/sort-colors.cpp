16581bfa70adfddc970db18b51bbc6e6fc93fc5b
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-02-24 16:25:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (42.24%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& v) {
+        int z = 0, f = 0 , s = v.size() - 1;
+        while(f <= s){
+            if(v[f] == 0){
+                swap(v[f],v[z]);
+                f++;z++;
+            }
+            else if(v[f] == 1){
+                f++;
+            }
+            else if(v[f] == 2) {
+                swap(v[f],v[s]);s--;
+            }
+        }
+    }
+};
