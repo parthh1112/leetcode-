14748c772cd59018e9328b00cb7528feb79a5428
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-09 21:27:21 +0530

    Time: 3 ms (60.80%) | Memory: 8.2 MB (92.23%) - LeetSync

---
+++ 390-elimination-game/elimination-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        int head = 1;
+        bool left = 1;
+        int rem = n;
+        int step = 1;
+        while(rem > 1){
+            if(left || rem%2 == 1){
+                head += step;
+            }
+
+            left = !left;
+            rem /= 2;
+            step *= 2;
+        }
+        return head;
+    }
+};
