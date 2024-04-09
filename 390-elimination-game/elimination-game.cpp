295da9c08e629bf29234095d6dc89937cbc188d8
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-09 21:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (60.80%) - LeetSync

--- 390-elimination-game/elimination-game.cpp
+++ 390-elimination-game/elimination-game.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        int head = 1;
+        int head = 1, rem = n, step = 1;
         bool left = 1;
-        int rem = n;
-        int step = 1;
         while(rem > 1){
             if(left || rem%2 == 1){
                 head += step;
