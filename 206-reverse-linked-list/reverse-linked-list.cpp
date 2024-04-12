edbd2ad56a44bc3878565452d3ac37271bb67251
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-12 12:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (66.62%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    void r(ListNode *&t,ListNode *&n){
-        if(!n)return ;
-        ListNode *e = n -> next;
-        n -> next = t;
-        t = n;
-        n = e;
-        r(t,n);
-    }
-    ListNode* reverseList(ListNode* h) {
-        if(h == NULL)return NULL;
-        ListNode *t = h,*n=h->next;
-        t -> next = NULL;
-        r(t,n);
-        return t;
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head -> next)return head;
+        ListNode *temp = head;
+        ListNode *pre = nullptr;
+        while(temp){  
+            ListNode *x = temp -> next;
+            temp -> next = pre;
+            pre = temp;
+            temp = x;
+        }
+        return pre;
     }
 };
