2019a690a5d194dafeaddd496182ee444191dc03
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-14 19:12:22 +0530

    Time: 5 ms (56.53%) | Memory: 15.2 MB (26.40%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int length(ListNode *head){
+        int cnt = 0;
+        ListNode *temp = head;
+        while(temp)temp = temp->next,cnt++;
+        return cnt;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if((!head) || (!head -> next) || (k == 0)){
+            return head;
+        }
+        int l = length(head);
+        if(k != 1 && (k % l == 0))return head;
+
+        ListNode *temp = head;
+        k = k % l;
+        int x = l - k;
+        while(x!=1){
+            x--;
+            temp = temp -> next;
+        }
+        ListNode *newHead = temp -> next;
+        temp -> next = nullptr;
+        temp = newHead;
+        while(temp->next)temp = temp -> next;
+        temp -> next = head;
+        return newHead;
+    }
+};
