53ed2970c7d343b31c9c612c28a742af49b824a3
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-20 17:39:04 +0530

    Time: 18 ms (11.19%) | Memory: 22.2 MB (13.22%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -5,11 +5,11 @@ int f = -1;
         if(!root)return 0;
         int l = findans(root->left);
         int r = findans(root->right);
-        f = max(f,l + r + 1);
+        f = max(f,l + r);
         return max(l,r)+1;
     }
     int diameterOfBinaryTree(TreeNode* root) {
         int ans = findans(root);
-        return f-1;
+        return f;
     }
 };
