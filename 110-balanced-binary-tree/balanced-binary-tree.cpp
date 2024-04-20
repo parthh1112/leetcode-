f9ae828e12f72284cfa26c5d8f2505ad38ffb854
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-20 12:37:23 +0530

    Time: 8 ms (73.13%) | Memory: 21.5 MB (77.01%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int ch (TreeNode *root,bool &ans){
+        if(!root)return 0;
+        int l = ch(root->left,ans)+1;
+        int r = ch(root->right,ans)+1;
+        if(abs(l-r) >= 2){
+            ans = 0;
+        } 
+        return max(l,r);
+    }
+    bool isBalanced(TreeNode* root) {
+        bool ans = 1;
+        int x = ch(root,ans);
+        return ans;
+    }
+};
