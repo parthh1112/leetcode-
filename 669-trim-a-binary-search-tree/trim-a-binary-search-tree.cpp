0df34753e6bbbc127931c2397fc52d4e7cdaa3d9
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-04-28 03:32:27 +0530

    Time: 14 ms (33.38%) | Memory: 22.5 MB (73.26%) - LeetSync

---
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int l, int r) {
+        if(!root)return root;
+
+        if(root->val >= l && root->val <= r){ // valid
+            root->left = trimBST(root->left,l,r);
+            root->right = trimBST(root->right,l,r);
+        }
+        else { // invalid
+            if(root->val < l){
+                return trimBST(root->right,l,r);
+            }
+            else {
+                return trimBST(root->left,l,r);
+            }
+        }
+        return root;        
+    }
+};
