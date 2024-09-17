daf98fde0282567e335edfb7f45030495bf3980a
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-09-17 08:08:09 +0530

    Time: 95 ms (95.26%) | Memory: 101.4 MB (51.54%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<int>&v, int x) {
+        int summ = 0;
+        for(auto i : v)summ += i;
+        if(summ == x)return v.size();
+        int window = summ - x;
+        // now find the max window size of sum window
+        int i = 0, j = 0, sum = 0, w = -1, n = v.size(); 
+        while(i < n){ 
+            sum += v[i];
+            while(sum > window && j < n){
+                sum -= v[j];
+                j++;
+            }
+            if(sum == window && j < n){
+                w = max(w,i - j + 1);
+                sum -= v[j];
+                j++;
+            }
+            i++;
+        }
+        cout<<w;
+        return (w == -1) ? w : n-w;
+    }
+};
