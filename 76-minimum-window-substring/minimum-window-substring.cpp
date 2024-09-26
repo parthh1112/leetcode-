b73fd360ce08d1fc69684307054cca35583fe46a
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-09-26 19:26:46 +0530

    Time: 13 ms (69.21%) | Memory: 10.6 MB (67.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,38 +1,35 @@
 class Solution {
-public:
-    int chk(map<char,int> &u,map<char,int> &v){ 
-        int cnt = 0;
-        for(auto j : v){
-            if(j.second >= u[j.first])cnt++;
-            else return 0;
-        }
-        return cnt;
-    }
-    string minWindow(string s, string p) {
-        int ansl = 1000000,ansr = 0;
-        map<char,int> u,o;
-        for(auto i : p)o[i]++;
-        int l =0,r=0,n=s.size();
-        vector<int> v;
-        int q = 0;
-        while(r < n){
-            if(o.find(s[r]) != o.end()){
-                v.push_back(r);
-                u[s[r]]++;
-                int cnt = chk(o,u);
-                while(cnt == o.size()){
-                    int x = v[q];
-                    int y = r;
-                    if(abs(ansl-ansr) > abs(x-y)){
-                        ansl = x;ansr = r;
-                    }
-                    u[s[v[q++]]]--;
-                    cnt = chk(o,u);
+public: 
+    string minWindow(string s, string t) { 
+        int lAns = INT_MAX, tCnt = 0, i = 0, j = 0, n = s.size(), m = t.size(), fl = -1, fr = -1;
+        if(m > n) return "";
+        vector<int> hash(256, 0), data(256, 0); 
+        for(auto c : t) hash[c]++;
+        int cnt = 0; 
+        for(int i = 0; i < 256; i++){
+            if(hash[i] > 0) cnt++;
+        }          
+        string ans = ""; 
+        while(i < n){ 
+            if(hash[s[i]] > 0){
+                data[s[i]]++;
+                if(data[s[i]] == hash[s[i]]) tCnt++;
+            } 
+            while(tCnt == cnt){
+                if(i - j + 1 < lAns){
+                    fl = j;
+                    fr = i; 
+                    lAns = i - j + 1;
+                } 
+                if(hash[s[j]] > 0){
+                    data[s[j]]--;
+                    if(data[s[j]] < hash[s[j]]) tCnt--;
                 }
-            }
-            r++;
+                j++;
+            }            
+            i++;
         } 
-        if(ansl == 1000000)return "";
-        return s.substr(ansl,ansr-ansl+1);
+        if(fl == -1 || fr == -1) return "";
+        return s.substr(fl, fr - fl + 1);
     }
 };
