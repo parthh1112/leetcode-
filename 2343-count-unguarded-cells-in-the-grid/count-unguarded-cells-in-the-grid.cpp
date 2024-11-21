517d76b088f6fd8bcd22238e5aa8a533d073b19d
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2024-11-21 12:13:47 +0530

    Time: 243 ms (11.34%) | Memory: 222.7 MB (10.46%) - LeetSync

---
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& g, vector<vector<int>>& w) {
+        vector<vector<int>> v(m,vector<int> (n,0));
+        for(auto i : w) v[i[0]][i[1]] = -1;
+        for(auto i : g) v[i[0]][i[1]] = -1;
+        
+        for(auto i : g){
+            int ii = i[0];
+            int jj = i[1]; 
+            for(int i=ii-1;i>=0;i--){
+                if(v[i][jj] == -1)break;
+                v[i][jj] = 1;
+            }
+            for(int i=ii+1;i<m;i++){
+                if(v[i][jj] == -1)break;
+                v[i][jj] = 1;
+            }
+            for(int j=jj-1;j>=0;j--){
+                if(v[ii][j] == -1)break;
+                v[ii][j] = 1;
+            }
+            for(int j=jj+1;j<n;j++){
+                if(v[ii][j] == -1)break;
+                v[ii][j] = 1;
+            }
+        }
+        int ans = 0;
+        for(auto i : v){
+            for(auto j : i){
+                if(j == 0)ans++;
+            }
+        }
+    return ans;
+    }
+};
