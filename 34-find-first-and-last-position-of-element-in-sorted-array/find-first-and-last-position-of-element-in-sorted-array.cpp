ce22c647cbe8c6a8b138196437a6b9b4a0202a34
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2025-06-27 12:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.41%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int firstOcc(vector<int> &v,int t){
+        int l = 0, r = v.size() - 1;
+        int ans = r;
+        while(l <= r){
+            int m = l + ((r - l) / 2);
+            if(v[m] < t){
+                l = m + 1;
+            }
+            else if(v[m] > t){
+                ans = m;
+                r = m - 1;
+            }
+            else{
+                ans = m;
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+    int sec(vector<int> &v,int t){
+        int l = 0, r = v.size() - 1;
+        int ans = r+1;
+        while(l <= r){
+            int m = l + ((r - l) / 2);
+            if(v[m] < t){
+                l = m + 1;
+            }
+            else if(v[m] > t){
+                ans = m;
+                r = m - 1;
+            }
+            else{
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& v, int t) {
+        if(v.size() == 0)return {-1,-1};
+        vector<int> ans = {-1,-1};
+        int f = firstOcc(v,t);
+        if(v[f] != t)return ans;
+        cout<<f<<endl;
+        // return {0,0};
+        ans[0] = f;
+        int s = sec(v,t);
+        if(v[s-1] != t){
+            return {f,f};
+        }
+        return {f,s-1}; 
+    }
+};
