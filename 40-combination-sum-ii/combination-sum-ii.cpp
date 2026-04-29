6fa38d22643451d9b4f427646c6c7c9be500749e
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-04-29 06:11:03 +0530

    Time: 31 ms (5.27%) | Memory: 20.4 MB (5.18%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public: 
+    void f(vector<int> &v,int t,int ind,vector<int> help,vector<vector<int>> &ans){
+        if(t < 0)return ;
+        if(t == 0){
+            ans.push_back(help);
+            return ;
+        }
+        if(ind == v.size())return ;
+        for(int i=ind;i<v.size();i++){ 
+            help.push_back(v[i]);
+            f(v,t-v[i],i+1,help,ans);
+            help.pop_back();
+            while(i < v.size()-1 &&  v[i] == v[i+1])i++;
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& v, int t) {
+        sort(v.begin(),v.end());
+        vector<vector<int>> ans;
+        vector<int> help;
+        f(v,t,0,help,ans);
+        return ans;
+    }
+};
