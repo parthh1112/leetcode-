ebb8b0953504894070197f57ef41fbd748c3435b
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-04-29 22:43:28 +0530

    Time: 4 ms (13.51%) | Memory: 16.6 MB (8.71%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void f(vector<int> &v,int i,vector<int> help,vector<vector<int>> &ans){
-        if(i >= v.size()){
+        if(i == v.size()){
             ans.push_back(help);
             return ;
         }
         help.push_back(v[i]);
-        f(v, i+1, help, ans);
-        help.pop_back(); 
-        int j = i;
-        while(j + 1 < v.size() && v[j] == v[j+1]) j++; 
-        f(v, j+1, help, ans); 
+        f(v,i+1,help,ans);
+        help.pop_back();
+        while(i < v.size() - 1 && v[i] == v[i+1])i++;
+        f(v,i+1,help,ans);
     }
     vector<vector<int>> subsets(vector<int>& v) {sort(v.begin(),v.end());
         vector<vector<int>>  ans;
