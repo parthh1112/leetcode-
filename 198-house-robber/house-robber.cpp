8779a34ee67133a043f9e8ab678d90291a55e282
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-02 10:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (87.86%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -9,15 +9,11 @@ public:
     }
     int rob(vector<int>& nums) {
         if(nums.size() == 1)return nums[0];
-        vector<int> dp(nums.size() , 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1],nums[0]);
-        int ans = max(dp[0],dp[1]);
-        for(int i=2;i<nums.size();i++){
-            int l = nums[i] + dp[i-2];
-            int r = dp[i - 1];
-            ans = max(l,r);
-            dp[i] = ans;
+        int prepre = nums[0], pre = max(nums[1],nums[0]), ans = max(prepre,pre);
+        for(int i=2;i<nums.size();i++){ 
+            ans = max(nums[i] + prepre,pre);
+            prepre = pre;
+            pre = ans;
         }
         return ans;
     }
