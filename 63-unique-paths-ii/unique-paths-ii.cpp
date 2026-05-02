219b74c3f8df436dd627ddd7e0c0fc3ccd8c7ffd
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-02 22:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (28.56%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int f(int i, int j, vector<vector<int>>& g, vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
         if(i == 0 && j == 0) return 1;
-        if(g[i][j] == 1) return 0;
+        if(i < 0 || j < 0 || g[i][j] == 1) return 0; 
         if(dp[i][j] != -1) return dp[i][j];
         int up = f(i - 1, j, g, dp);
         int left = f(i, j - 1, g, dp);
