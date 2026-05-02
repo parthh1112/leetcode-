054ebe3a24f06c912da6fd54ce914d4e35ebb99b
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-02 10:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (20.58%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,14 @@
 class Solution {
-public: 
-    int rob(vector<int>& v) { 
-        int pre  = v[0];
-        int pre2  = -1;
-        for(int i=1;i<v.size();i++){
-            int left = v[i];
-            if(i > 1) left += pre2;
-            int right = pre;
-            int ctrMAX = max(left,right); 
-            pre2 = pre;
-            pre = ctrMAX; 
-        } 
-        return pre;
+public:
+    int f(int i, vector<int> &nums,vector<int> &dp){
+        if(i >= nums.size())return 0;
+        if(dp[i] != -1)return dp[i];
+        int l = nums[i] + f(i + 2, nums,dp);
+        int r = f(i + 1, nums,dp);
+        return dp[i] =  max(l,r);
+    }
+    int rob(vector<int>& nums) {
+        vector<int> m(nums.size() , -1);
+        return f(0,nums,m);
     }
 };
