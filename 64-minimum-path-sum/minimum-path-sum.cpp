faacb2e4b51900cabcdec621ff47de9318da3a9d
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-03 01:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (57.09%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,15 +1,5 @@
 class Solution {
-public:
-    long long int f(int i, int j, vector<vector<int>> &g, vector<vector<int>>&d){
-        if(i < 0 || j < 0) return INT_MAX;
-        if(i == 0 && j == 0){
-            return g[0][0];
-        } 
-        if(d[i][j] != -1)return d[i][j];
-        long long int up = f(i - 1, j, g, d) + g[i][j];
-        long long int left = f(i, j - 1, g, d) + g[i][j]; 
-        return d[i][j] = min(left, up);
-    }
+public: 
     int minPathSum(vector<vector<int>>& g) {
         vector<vector<int>> d(g.size(),vector<int> (g[0].size(), 0));
         d[0][0] = g[0][0];
