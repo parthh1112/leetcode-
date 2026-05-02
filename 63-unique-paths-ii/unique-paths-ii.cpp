37f76fc0b91d0077b8798c4d1123264ea757f8ff
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-02 22:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (5.99%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -10,7 +10,17 @@ public:
     }
     int uniquePathsWithObstacles(vector<vector<int>>& g) {
         if(g[0][0] == 1 || g[g.size() - 1][g[0].size() - 1])return 0;
-        vector<vector<int>> dp(g.size(),vector<int> (g[0].size(), -1));
-        return f(g.size() - 1, g[0].size() - 1, g, dp);        
+        vector<vector<int>> dp(g.size(),vector<int> (g[0].size(), 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < g.size(); i++){
+            for(int j = 0; j < g[0].size(); j++){
+                if(i == 0 && j == 0)continue;
+                int up = 0, left = 0;
+                if(i > 0 && g[i][j] != 1) up = dp[i - 1][j];
+                if(j > 0 && g[i][j] != 1) left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[g.size() - 1][g[0].size() - 1];
     }
 };
