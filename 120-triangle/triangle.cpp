9a0e018f1dda8987b2cfc7db2ffebbab393e93d8
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-03 17:20:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (96.29%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,11 +2,7 @@ class Solution {
 public: 
     int minimumTotal(vector<vector<int>>& t) { 
         for(int i = t.size() - 2; i >= 0; i--){
-            for(int j = 0; j < t[i].size(); j++){
-                int one = t[i + 1][j] + t[i][j];
-                int second = t[i + 1][j + 1] + t[i][j];
-                t[i][j] = min(one,second);
-            }
+            for(int j = 0; j < t[i].size(); j++) t[i][j] = min(t[i + 1][j] + t[i][j], t[i + 1][j + 1] + t[i][j]);
         }
         return t[0][0];
     }
