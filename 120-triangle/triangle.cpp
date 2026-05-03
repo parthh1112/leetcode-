0c3595d753130194250255814b4db4cfcdf8cb2d
Parth Gupta <95039857+parthh1112@users.noreply.github.com>
2026-05-03 17:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (49.42%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,22 +1,22 @@
-#define ll long long int
-#define pb push_back
-#define vi vector<int>
-#define vvi vector<vi>
 class Solution {
 public:
-int minimumTotal(vvi &v)
-{ 
-    vvi dp(v.size(), vi(v[v.size() - 1].size(), 0));
-    dp[v.size() - 1] = v[v.size() - 1];
-    for (int i = v.size() - 2; i >= 0; i--)
-    {
-        for (int j = 0; j < v[i].size(); j++)
-        {
-            ll l = v[i][j] + dp[i + 1][j];
-            ll r = v[i][j] + dp[i + 1][j + 1];
-            dp[i][j] = min(l, r);
-        }
+    int f(int  row, int i, vector<vector<int>> &t,vector<vector<int>> &d){
+        if(row == t.size() - 1)return t[t.size() - 1][i];
+        if(d[row][i] != -1)return d[row][i];
+        int one = f(row + 1, i ,t, d) + t[row][i];
+        int second = f(row + 1, i + 1, t, d) + t[row][i]; 
+        return d[row][i] =  min(one,second);
     }
-    return dp[0][0];
-}
+    int minimumTotal(vector<vector<int>>& t) {
+        vector<vector<int>> d(t.size(), vector<int> (t.size(), -1));
+        d[t.size() - 1] = t[t.size() - 1]; 
+        for(int i = t.size() - 2; i >= 0; i--){
+            for(int j = 0; j < t[i].size(); j++){
+                int one = d[i + 1][j] + t[i][j];
+                int second = d[i + 1][j + 1] + t[i][j];
+                d[i][j] = min(one,second);
+            }
+        }
+        return d[0][0];
+    }
 };
